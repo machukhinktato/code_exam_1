281c95521007fecd0068485666deb6c4eba4e9a0
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-02-12 13:31:23 +0300

    done

--- case_3.py
+++ case_3.py
@@ -6,7 +6,7 @@ def gen_k_streak(user_pick = '3aabacbebebe'):
     k_qty = int(user_pick[0])
     k_streak = ''
 
-    if len(string) < k_qty:
+    if len(string) < k_qty or len(set(string)) < k_qty:
         sys.exit('there is no enough unique characters')
 
     elif len(string) == k_qty and len(set(string)) == k_qty:
@@ -19,10 +19,8 @@ def gen_k_streak(user_pick = '3aabacbebebe'):
                 substring = string[elem:]
                 if len(substring) > len(k_streak):
                     k_streak = substring
-    
             else:
                 while len(set(substring)) > k_qty:
-
                     substring = substring[:-1]
                     if len(set(substring)) == k_qty and len(substring) > len(k_streak):
                         k_streak = substring
