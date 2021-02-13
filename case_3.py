d85e4eac7f402504ca001fa89ad10865bbc6c306
machukhinktato <tarabrinmv@gmail.com>
2021-02-13 15:50:06 +0300

    all works project in  progress

--- case_3.py
+++ case_3.py
@@ -1,30 +1,34 @@
 import sys
 
 
-def gen_k_streak(user_pick = '3aabacbebebe'):
-    string = ''.join(user_pick[1:])
-    k_qty = int(user_pick[0])
-    k_streak = ''
-
+# def k_unique_characters(value='2aabbcbbbadef'):
+def k_unique_characters(value='3aabacbebebe'):
+    string = ''.join(value[1:])
+    k_qty = int(value[0])
+    k_chars = [0] * len(string)
+    data, result = [], []
     if len(string) < k_qty or len(set(string)) < k_qty:
         sys.exit('there is no enough unique characters')
-
     elif len(string) == k_qty and len(set(string)) == k_qty:
         return string
-    
     else:
-        for elem in range(len(string)):
-            substring = string[elem:]
-            if len(set(substring)) == k_qty:
-                substring = string[elem:]
-                if len(substring) > len(k_streak):
-                    k_streak = substring
-            else:
-                while len(set(substring)) > k_qty:
-                    substring = substring[:-1]
-                    if len(set(substring)) == k_qty and len(substring) > len(k_streak):
-                        k_streak = substring
+        for elem in range(len(k_chars)):
+            k_chars[elem] = string[elem]
+        for idx in range(len(string)):
+            data.clear()
+            for _while in range(len(string)):
+                try:
+                    data.append(k_chars[idx])
+                except:
+                    pass
+                idx += 1
+                if len(set(data)) > k_qty:
+                    data[-1] = ''
+                    break
+            if len(data) > len(result):
+                result = ''.join([char for char in data])
 
-    return k_streak
+        return result
 
-print(gen_k_streak())
+
+print(k_unique_characters())
