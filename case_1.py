3f064bc8539437f96ada8bd96a4abd31d83c3236
machukhinktato <tarabrinmv@gmail.com>
2021-02-14 10:02:16 +0300

    all works project in  progress

---
+++ case_1.py
@@ -0,0 +1,34 @@
+import re
+
+
+def time_difference(data=['10:00am', '11:45', '5^00am', '12^01am']):
+    """ функция высчитывает меньший временной диапозон в предоставленном списке"""
+    time_list = []
+    for elem in data:
+        if 'pm' in elem:
+            time_list.append(handling(elem))
+        else:
+            time_list.append((handling(elem, status='pm')))
+    return smallest_diff(sorted(time_list))
+
+
+def handling(elem, status='pm'):
+    match = re.findall(r'\d+', elem)
+    if status == 'pm' or int(match[0]) == 12:
+        match = int(match[0]) + 12, int(match[1])
+    match = int(match[0]) * 60 + int(match[1])
+    return match
+
+
+def smallest_diff(value):
+    result = []
+    for elem in range(len(value)):
+        try:
+            result.append(value[elem+1] - value[elem])
+        except:
+            pass
+    return min(result)
+
+
+if __name__ == '__main__':
+    print(time_difference())
